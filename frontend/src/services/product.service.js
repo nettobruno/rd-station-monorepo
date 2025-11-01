5c4e67e0fab19ec7c60ad4b77501f9c3de1009ec
Bruno Netto <brunonettomac@outlook.com>
2025-11-01 20:28:18 -0300

    feat: git commit -m "feat(api): configure dynamic base URL to consume new backend on Render

--- frontend/src/services/product.service.js
+++ frontend/src/services/product.service.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3001';
+const baseURL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "http://localhost:3001"; 
 
 const getProducts = async () => {
   try {
-    const response = await axios.get(`${baseURL}/products`);
+    const response = await axios.get(`${baseURL}/api/products`);
     return response.data;
   } catch (error) {
     console.error('Erro ao obter os produtos:', error);
